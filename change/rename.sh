dfd18cf47f06c26026898d4aaa69d3edb6f2ac50
aman150084 <aman150084@gmail.com>
2016-06-18 19:03:05 +0530

    initial

--- change/rename.sh
+++ change/rename.sh
@@ -1,1 +1,7 @@
 cd /home/amands/Github/digit/change/images
+for f in *.jpg
+do
+  cp "/home/amands/Github/digit/change/images/$f" "/home/amands/Github/digit/change/image_dont_touch"
+  mv "/home/amands/Github/digit/change/image_dont_touch/$f" "/home/amands/Github/digit/change/image_dont_touch/image.jpg"
+
+done
